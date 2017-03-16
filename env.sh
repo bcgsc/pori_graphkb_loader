0b567ec968c5a01bb1aaeede828ec09b8df48f08
Caralyn Reisle <creisle@bcgsc.ca>
2017-03-16 10:22:18 -0700

    initial commit

---
+++ env.sh
@@ -0,0 +1,6 @@
+# defaults just for getting things up and running. These values will not be retained when authentication is set up properly
+
+export ORIENTDB_PORT=2481
+export ORIENTDB_SERVER='localhost'
+export ORIENTDB_PASS='root'
+export ORIENTDB_USER='root'
