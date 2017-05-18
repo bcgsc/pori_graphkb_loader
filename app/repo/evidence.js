b6ac49cc9ae1e9643107c82b106b5727a9acd1c6
amirmhzadeh <amirmhzadeh@gmail.com>
2017-05-18 13:46:49 -0700

    KBDEV-16, KBDEV-9 | CHNG: as per Cara's comments, the createRecord() for Publication class has changed to prevent DuplicateKeyError when referencing different publications to one journal.

--- app/repo/evidence.js
+++ app/repo/evidence.js
@@ -41,46 +41,50 @@ class Publication extends KBVertex {
         } else if ((content.year < 1000) || (content.year > currYear('yyyy'))) {
             throw new AttributeError('publication year cannot be too old or in the future');
         }
-
         content.journal = journalClass.validateContent(content.journal);
         content.title = content.title.toLowerCase();
         if (content.doi != undefined || content.pmid != undefined) {
-            if (! content.doid === parseInt(content.doid, 10)) {
+            if (content.pmid !== parseInt(content.pmid, 10)) {
                 // if pmid is not an integer
                 throw new AttributeError('PMID must be an integer');
-            } else {
-                content.doi = content.doi.toLowerCase();
+            } else { 
+                content.doi = content.doi != undefined ? content.doi.toLowerCase() : undefined;
             }
         }
 
         return super.validateContent(content);
     }
 
-/*            var commit = this.dbClass.db
-                .let('journalName', (trs) => {
-                    return trs.create(journalClass.constructor.createType, journalClass.constructor.clsname).set(args.journal);
-                }).let('link', (trs) => {
-                    //connect the nodes
-                    const sub = Object.assign({}, args);
-                    delete sub.journal;
-                    return trs.create(this.constructor.createType, this.constructor.clsname).set(sub).set('journal = $journalName');
-                }).commit();
-            
-            commit.return('$link').one().then((record) => {
-                this.dbClass.db.record.get(record.journal).then((journalName) => {
-                    record.journal = journalName;
-                    resolve(record);
+    createRecord(opt, journalClass) {            
+        return new Promise((resolve, reject) => {
+            const args = this.validateContent(opt, journalClass);
+            journalClass.selectExactlyOne({name: args.journal.name})
+                .catch(NoResultFoundError, () => {
+                    return journalClass.createRecord(args.journal);
+                }).then((journalRecord) => {
+                    args.journal = journalRecord['@rid'];
+                    return this.dbClass.create(args)
+                        .then((record) => {
+                            this.dbClass.db.record.get(record.journal).then((journalName) => {
+                                record.journal = journalName;
+                                resolve(record);
+                            }).catch((error) => {
+                                reject(error);
+                            })
+                        })
+                        .catch((error) => {
+                            reject((error));
+                        })
                 }).catch((error) => {
                     reject(error);
                 });
-            }).catch((error) => {
-                reject(error);
-            });*/
+        });
+    }
 
     static createClass(db){
         return new Promise((resolve, reject) => {
             const props = [
-                {name: 'journal', type: 'link', mandatory: true, notNull: true, linkedClass: Journal.clsname},
+                {name: 'journal', type: 'link', mandatory: true, notNull: true, linkedClass: Evidence.clsname},
                 {name: 'year', type: 'integer', mandatory: true, notNull: true},
                 {name: 'title', type: 'string', mandatory: true, notNull: true},
                 {name: 'doi', type: 'string', mandatory: false},
@@ -93,40 +97,11 @@ class Publication extends KBVertex {
                 properties: ['deleted_at', 'journal', 'year', 'title'],
                 'class':  this.clsname
             }];
-            Base.createClass({db, clsname: this.clsname, superClasses: Evidence.clsname, properties: props, isAbstract: false, indices: idxs})
+            Base.createClass({db, clsname: this.clsname, superClasses: Evidence.clsname, properties: props, indices: idxs})
                 .then(() => {
                     return this.loadClass(db);
                 }).then((cls) => {
                     resolve(cls);
-                }).catch((error) => {
-                    reject(error);
-                });
-        });
-    }
-
-    createRecord(opt, journalClass) {            
-        return new Promise((resolve, reject) => {
-            const args = this.validateContent(opt, journalClass);
-            this.selectExactlyOne(args.journal)
-                .catch(NoResultFoundError, () => {
-                    console.log("THIS SHOULD ONLY APPEAR *ONCE*")
-                    return journalClass.createRecord(args.journal);
-                }).then((journalRecord) => {
-                    var commit = this.dbClass.db
-                        .let('link', (trs) => {
-                        //connect the nodes
-                        const sub = Object.assign({}, args);
-                        args.journal = journalRecord['@rid'];
-                        return trs.create(this.constructor.createType, this.constructor.clsname).set(args);
-                    }).commit();
-                    commit.return('$link').one().then((record) => {
-                        this.dbClass.db.record.get(record.journal).then((journalName) => {
-                            record.journal = journalName;
-                            resolve(record);
-                        }).catch((error) => {
-                            reject(error);
-                        });
-                    });
                 }).catch((error) => {
                     reject(error);
                 });
