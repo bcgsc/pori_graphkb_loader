50e5cccb6e36f67d3e7d330e67fceda038ae7c20
Caralyn Reisle <creisle@bcgsc.ca>
2017-06-22 10:18:28 -0700

    adding initial swagger spec defn file for auto-generating the swagger spec

---
+++ doc/swagger-jsdoc-defn.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+  info: { // API informations (required)
+      title: 'KnowledgeBase', // Title (required)
+      version: '1.0.0' // Version (required)
+    },
+  basePath: '/api', // Base path (optional)
+};
