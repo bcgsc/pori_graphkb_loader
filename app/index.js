487163118c12334e1f334ae45869720585a7a551
Caralyn Reisle <creisle@bcgsc.ca>
2018-03-16 15:42:51 -0700

    KBDEV-48: Added certificate generation to create the private key for encrypting tokens

--- app/index.js
+++ app/index.js
@@ -5,7 +5,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const defaultConf = require('./../config/default'); // get the database connection configuration
 const add_routes = require('./routes');
-const {connectServer, connectDB} = require('./repo/connect');
+const OrientDB  = require('orientjs');
+const {loadSchema} = require('./repo/schema');
+const https = require('https');
+const selfsigned = require('selfsigned');
+const auth = require('./middleware/auth');
 
 
 let orientServer, dbServer, appServer;
@@ -16,25 +20,61 @@ app.use(bodyParser.json());
 
 // set up the routes
 const router = express.Router();
+
 app.use('/api', router);
 router.use((req, res, next) => {
     console.log(`[${req.method}] ${req.url}`);
     next();
 });
+router.use(auth.checkToken)
 
-const listen = async (conf={app: {}}) => {
-    orientServer = await connectServer(Object.assign({}, defaultConf.server, conf.server));
-    dbServer = await connectDB(Object.assign({}, defaultConf.db, conf.db, {server: orientServer}));
+
+const connectDB = async (conf, verbose=true) => {
+    // set up the database server
+    const server = OrientDB({
+        host: conf.server.host,
+        HTTPport: conf.server.port,
+        username: conf.server.user,
+        password: conf.server.pass
+    });
+    if (verbose) {
+        console.log(`connecting to the database: ${conf.db.name} as ${conf.db.user}`);
+    }
+    let db;
+    try {
+        db = await server.use({name: conf.db.name, username: conf.db.user, password: conf.db.pass});
+        if (verbose) {
+            console.log(`loading the schema`);
+        }
+    } catch (err) {
+        server.close();
+        throw err;
+    }
+    const schema = await loadSchema(db, verbose);
+    // create the admin user
+    return {server, db, schema};
+};
+
+const listen = async (conf={app: {}}, verbose=false) => {
+    // connect to the database
+    const {server, db, schema} = await connectDB(conf, verbose);
+    orientServer = server;
+    dbServer = db;
+    // create the authentication certificate for managing tokens
+    if (! auth.keys.key ) {
+        const keys = await selfsigned.generate();
+        auth.keys.key = keys.private;
+    }
     // add the db connection reference to the routes
-    add_routes(router, dbServer);
+    add_routes({router, db, schema});
     // last catch any errors for undefined routes. all actual routes should be defined above
     app.use((req, res) => {
         res.status(404);
         res.send({error: 'Not Found'});
     });
+    //appServer = await https.createServer({cert: keys.cert, key: keys.private, rejectUnauthorized: false}, app).listen(conf.app.port || defaultConf.app.port);
     appServer = await app.listen(conf.app.port || defaultConf.app.port);
     console.log('started application server at:', appServer.address().port);
-
 }
 
 const close = async () => {
---
+++ app/middleware/auth.js
@@ -0,0 +1,30 @@
+const HTTP_STATUS = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+
+
+const keys = {};
+const TOKEN_TIMEOUT = 120;
+/*
+ * TODO: autheticate the header token
+ * - check that the token is valid/active
+ * - check the user is allowed permission to the given endpoint
+ */
+const checkToken = async (req, res, next) => {
+    const token = req.header('Authorization');
+    if (token === undefined) {
+        return res.status(HTTP_STATUS.UNAUTHORIZED).json({message: 'did not find authorized token', type: 'PermissionError'});
+    }
+    try {
+        const decoded = jwt.verify(token, keys.key);
+        req.user = decoded.user;
+    } catch (err) {
+        return res.status(HTTP_STATUS.UNAUTHORIZED).json({message: 'bad token', type: 'PermissionError'});
+    }
+    next();
+};
+
+const generateToken = async (user, expires) => {
+    return await jwt.sign(user, keys.key, {expiresIn: expires || TOKEN_TIMEOUT});
+};
+
+module.exports = {generateToken, checkToken, keys};
