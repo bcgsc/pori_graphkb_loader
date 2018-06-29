591a58b4cdb151e84ec576e24b9358e3e2eeba50
Caralyn Reisle <creisle@bcgsc.ca>
2018-06-29 15:46:21 -0700

    Bumped version number to 0.0.8. Let DATABASE_NAME env var act as db name suffix for kbapi_ so that the db version does not always have to match the api version

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const OrientDB  = require('orientjs');
 
 //process.on('uncaughtException', app.close);
 let app;
-conf.db.name = `${process.env.DATABASE_NAME || 'api_test'}_v${process.env.npm_package_version || 'test'}`;
+conf.db.name = `kbapi_${process.env.DATABASE_NAME ? process.env.DATABASE_NAME : `v${process.env.npm_package_version}` }`;
 delete conf.port;
 
 (async () => {
