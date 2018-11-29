08d9fa4ac484ba3916ea07432237c6a0c69ca4b4
Caralyn Reisle <creisle@bcgsc.ca>
2018-11-29 11:26:19 -0800

    Default to 2.2.37 odb server

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const dbName = 'test_empty';
 const server = {
     pass: process.env.DATABASE_SERVER_PASS || 'root',
     user: process.env.DATABASE_SERVER_USER || 'root',
-    port: process.env.DATABASE_PORT || 2424,
+    port: process.env.DATABASE_PORT || 2425,
     host: process.env.DATABASE_HOST || 'orientdb02.bcgsc.ca'
 };
 
