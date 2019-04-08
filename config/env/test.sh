27a873fd7fcc2351afb5132c0db8087a2961d300
Caralyn Reisle <creisle@bcgsc.ca>
2019-04-08 15:28:21 -0700

    Remove unnecessary sh files

--- config/env/test.sh
+++
@@ -1,7 +0,0 @@
-export DB_CREATE=1
-export DISABLE_AUTH=0
-# use the test keycloak server
-export KEYCLOAK_KEYFILE=keycloak-dev.id_rsa.pub
-export KEYCLOAK_URI=http://ga4ghdev01.bcgsc.ca:8080/auth/realms/TestKB/protocol/openid-connect/token
-export LOG_LEVEL=debug
-export NODE_ENV=test
