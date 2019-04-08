6555e2a9386ff23d0547bc356ed79668b9bc4ea1
Caralyn Reisle <creisle@bcgsc.ca>
2019-04-08 15:18:58 -0700

    KBDEV-471: Use namespace for ENV vars

--- config/config.js
+++ config/config.js
@@ -1,42 +1,82 @@
-const {ORIENTDB_HOME} = process.env;
+const {NODE_ENV = 'production'} = process.env;
 
 const dbName = process.env.DB_NAME || `kbapi_v${process.env.npm_package_version}`;
+const ENV_PREFIX = 'GKB_';
 
-const create = () => {
+const create = (envName = NODE_ENV) => {
+    // set the configuration defaults
     const server = {
-        pass: process.env.DBS_PASS || 'root',
-        user: process.env.DBS_USER || 'root',
-        port: process.env.DB_PORT || 2426,
-        host: process.env.DB_HOST || 'orientdb02.bcgsc.ca'
+        pass: 'root',
+        user: 'root',
+        port: 2426,
+        host: 'orientdb02.bcgsc.ca'
     };
 
     const db = {
         name: dbName,
-        url: `plocal:${ORIENTDB_HOME}/databases/${dbName}`,
-        pass: process.env.DB_PASS || 'admin',
-        user: process.env.DB_USER || 'admin',
-        host: server.host,
-        port: server.port,
-        create: process.env.DB_CREATE === '1',
-        migrate: process.env.DB_MIGRATE === '1'
+        pass: 'admin',
+        user: 'admin',
+        create: false,
+        migrate: false
     };
+
+    if (envName !== 'production') {
+        db.create = true;
+        db.migrate = false;
+    }
 
     const keycloak = {
-        uri: process.env.KEYCLOAK_URI || 'https://sso.bcgsc.ca/auth/realms/GSC/protocol/openid-connect/token',
-        clientID: process.env.KEYCLOAK_CLIENTID || 'GraphKB',
-        publicKeyFile: process.env.KEYCLOAK_KEYFILE || 'keycloak.id_rsa.pub',
-        role: process.env.KEYCLOAK_ROLE || 'GraphKB'
+        uri: 'https://sso.bcgsc.ca/auth/realms/GSC/protocol/openid-connect/token',
+        clientID: 'GraphKB',
+        publicKeyFile: 'keycloak.id_rsa.pub',
+        role: 'GraphKB'
     };
 
-    return {
-        server,
+    if (envName !== 'production') {
+        keycloak.uri = 'http://ga4ghdev01.bcgsc.ca:8080/auth/realms/TestKB/protocol/openid-connect/token';
+        keycloak.publicKeyFile = 'keycloak-dev.id_rsa.pub';
+    }
+
+    const app = {port: 8080};
+
+    const config = {
+        privateKeyFile: 'id_rsa',
+        disableAuth: false,
+        createUser: true,
         db,
-        app: {port: process.env.PORT || 8080},
-        privateKeyFile: process.env.KEY_FILE || 'id_rsa',
-        disableAuth: process.env.DISABLE_AUTH === '1',
+        server,
         keycloak,
-        createUser: true
+        app
     };
+
+    if (envName === 'local') {
+        config.disableAuth = true;
+    }
+
+    // override any defaults with env variables when set
+    for (const [obj, key, envVarName] of [
+        [config, 'privateKeyFile', 'KEY_FILE'],
+        [config, 'disableAuth', 'DISABLE_AUTH'],
+        [config, 'createUser', 'CREATE_USER'],
+        [keycloak, 'uri', 'KEYCLOAK_URI'],
+        [keycloak, 'clientID', 'KEYCLOAK_CLIENT_ID'],
+        [keycloak, 'publicKeyFile', 'KEYCLOAK_KEY_FILE'],
+        [keycloak, 'role', 'KEYCLOAK_ROLE'],
+        [app, 'port', 'PORT'],
+        [db, 'name', 'DB_NAME'],
+        [db, 'user', 'DB_USER'],
+        [db, 'pass', 'DB_PASS'],
+        [server, 'host', 'DB_HOST'],
+        [server, 'port', 'DB_PORT'],
+        [server, 'user', 'DBS_USER'],
+        [server, 'pass', 'DBS_PASS']
+    ]) {
+        const envKey = `${ENV_PREFIX}${envVarName}`;
+        if (process.env[envKey] !== undefined) {
+            obj[key] = process.env[envKey];
+        }
+    }
+    return config;
 };
 
 module.exports = create;
