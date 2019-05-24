0b4e019d34f3769e61f78f744fe5d2cbb06920f3
Caralyn Reisle <creisle@bcgsc.ca>
2019-05-24 12:35:22 -0700

    Update tests to store parsed variant

--- test/ipr.js
+++ test/ipr.js
@@ -15,8 +15,23 @@ describe('convertDeprecatedSyntax', () => {
     });
     test('fusion with specific exons', () => {
         const result = convertDeprecatedSyntax('(CLTC,ALK):fusion(e.31,e.20)');
+        const parsed = {
+            break1Repr: 'e.31',
+            break1Start: {
+                '@class': 'ExonicPosition',
+                pos: 31
+            },
+            break2Repr: 'e.20',
+            break2Start: {
+                '@class': 'ExonicPosition',
+                pos: 20
+            },
+            reference1: 'CLTC',
+            reference2: 'ALK',
+            type: 'fusion'
+        };
         expect(result).toEqual({
-            positional: '(CLTC,ALK):fusion(e.31,e.20)'
+            positional: parsed
         });
     });
     test('CNV_ERBB2_amplification_na', () => {
@@ -25,7 +40,20 @@ describe('convertDeprecatedSyntax', () => {
     });
     test('MUT_ARAF:p.S214A', () => {
         const result = convertDeprecatedSyntax('MUT_ARAF:p.S214A');
-        expect(result).toEqual({positional: 'ARAF:p.S214A'});
+        const parsed = {
+            break1Repr: 'p.S214',
+            break1Start: {
+                '@class': 'ProteinPosition',
+                pos: 214,
+                refAA: 'S'
+            },
+            refSeq: 'S',
+            reference1: 'ARAF',
+            type: 'substitution',
+            untemplatedSeq: 'A',
+            untemplatedSeqSize: 1
+        };
+        expect(result).toEqual({positional: parsed});
     });
     test('MUT_ERBB2_any', () => {
         const result = convertDeprecatedSyntax('MUT_ERBB2_any');
