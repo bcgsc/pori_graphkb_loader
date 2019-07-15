a7fc2e68f7c1e986da63fa1210bd9308ff7010b7
Caralyn Reisle <creisle@bcgsc.ca>
2019-07-15 11:24:11 -0700

    KBDEV-496: Allow columns to be null

--- src/drug_ontology.js
+++ src/drug_ontology.js
@@ -3,7 +3,7 @@
  * @module importer/drug_ontology
  */
 
-const {loadDelimToJson, rid} = require('./util');
+const {loadDelimToJson, rid, orderPreferredOntologyTerms} = require('./util');
 const {logger} = require('./logging');
 const {SOURCE_DEFN: {name: drugbankName}} = require('./drugbank');
 const {SOURCE_DEFN: {name: chemblName}} = require('./chembl');
@@ -13,7 +13,8 @@ const HEADER = {
     parent: 'Class_1',
     grandparent1: 'Class_2',
     grandparent2: 'Class_3_pathway',
-    drugbank: 'DrugBankID'
+    drugbank: 'DrugBankID',
+    alias: 'alias'
 };
 
 const SOURCE_DEFN = {
@@ -25,11 +26,15 @@ const SOURCE_DEFN = {
  * Try to extact match a drugbank/chembl record. If there isn't one then add this name as a new record instead
  */
 const getDrugOrAdd = async (conn, source, name) => {
+    if (!name.trim()) {
+        return null;
+    }
     let record;
     try {
         record = await conn.getUniqueRecordBy({
             endpoint: 'therapies',
-            where: {source: {name: drugbankName}, name}
+            where: {source: {name: drugbankName}, name},
+            sort: orderPreferredOntologyTerms
         });
         return record;
     } catch (err) {}
@@ -37,7 +42,8 @@ const getDrugOrAdd = async (conn, source, name) => {
     try {
         record = await conn.getUniqueRecordBy({
             endpoint: 'therapies',
-            where: {source: {name: chemblName}, name}
+            where: {source: {name: chemblName}, name},
+            sort: orderPreferredOntologyTerms
         });
         return record;
     } catch (err) {}
@@ -47,6 +53,48 @@ const getDrugOrAdd = async (conn, source, name) => {
         content: {name, sourceId: name, source: rid(source)},
         existsOk: true
     });
+};
+
+
+/**
+ * Try to extact match a drugbank/chembl record. If there isn't one then add this name as a new record instead
+ */
+const addDrugClass = async (conn, source, name, tags) => {
+    if (!name.trim()) {
+        return null;
+    }
+
+    const record = await conn.addRecord({
+        endpoint: 'therapies',
+        content: {
+            name,
+            sourceId: name,
+            source: rid(source),
+            subsets: tags
+        },
+        existsOk: true,
+        fetchConditions: {
+            name,
+            sourceId: name,
+            source: rid(source)
+        }
+    });
+
+    // link to drugs with exact name matches
+    try {
+        const drugbankDrug = await conn.getUniqueRecordBy({
+            endpoint: 'therapies',
+            where: {source: {name: drugbankName}, name},
+            sort: orderPreferredOntologyTerms
+        });
+        await conn.addRecord({
+            endpoint: 'crossreferenceof',
+            content: {out: rid(record), in: rid(drugbankDrug), source: rid(source)},
+            existsOk: true,
+            fetchExistsing: false
+        });
+    } catch (err) {}
+    return record;
 };
 
 
@@ -73,17 +121,29 @@ const uploadFile = async (opt) => {
     for (const record of content) {
         logger.info(`processing ${record[HEADER.name]}`);
         try {
-            const [drug, parent, grandparent1, grandparent2] = await Promise.all([
-                record[HEADER.name],
-                record[HEADER.parent],
-                record[HEADER.grandparent1],
-                record[HEADER.grandparent2]
-            ].map(async name => getDrugOrAdd(conn, source, name)));
+            const drug = await getDrugOrAdd(conn, source, record[HEADER.name]);
+            const [parent, grandparent1, grandparent2] = await Promise.all([
+                addDrugClass(conn, source, record[HEADER.parent], ['class1']),
+                addDrugClass(conn, source, record[HEADER.grandparent1], ['class2']),
+                addDrugClass(conn, source, record[HEADER.grandparent2], ['class3_pathway'])
+            ]);
+            const aliases = await Promise.all(
+                record[HEADER.alias].split(/\s*,\s*/)
+                    .filter(term => term)
+                    .map(async term => getDrugOrAdd(conn, source, term))
+            );
+            // link the drug to its alias terms
+            await Promise.all(aliases.map(async alias => conn.addRecord({
+                endpoint: 'aliasof',
+                content: {out: rid(alias), in: rid(drug), source: rid(source)},
+                existsOk: true
+            })));
             // get the mapped drugbank drug
             if (/^DB\d+$/i.exec(record[HEADER.drugbank])) {
                 const dbDrug = rid(await conn.getUniqueRecordBy({
                     endpoint: 'therapies',
-                    where: {source: {name: drugbankName}, sourceId: record[HEADER.drugbank]}
+                    where: {source: {name: drugbankName}, sourceId: record[HEADER.drugbank]},
+                    sort: orderPreferredOntologyTerms
                 }));
                 // now link the records together
                 if (dbDrug !== rid(drug)) {
@@ -97,29 +157,31 @@ const uploadFile = async (opt) => {
             } else {
                 logger.info('No corresponding drugbank drug');
             }
-            if (rid(drug) !== rid(parent)) {
-                await conn.addRecord({
-                    endpoint: 'subclassof',
-                    content: {out: rid(drug), in: rid(parent), source},
-                    existsOk: true,
-                    fetchExistsing: false
-                });
-            }
-            if (rid(parent) !== rid(grandparent1)) {
-                await conn.addRecord({
-                    endpoint: 'subclassof',
-                    content: {out: rid(parent), in: rid(grandparent1), source},
-                    existsOk: true,
-                    fetchExistsing: false
-                });
-            }
-            if (rid(parent) !== rid(grandparent2)) {
-                await conn.addRecord({
-                    endpoint: 'subclassof',
-                    content: {out: rid(parent), in: rid(grandparent2), source},
-                    existsOk: true,
-                    fetchExistsing: false
-                });
+            if (parent) {
+                if (rid(drug) !== rid(parent)) {
+                    await conn.addRecord({
+                        endpoint: 'subclassof',
+                        content: {out: rid(drug), in: rid(parent), source},
+                        existsOk: true,
+                        fetchExistsing: false
+                    });
+                }
+                if (grandparent1 && rid(parent) !== rid(grandparent1)) {
+                    await conn.addRecord({
+                        endpoint: 'subclassof',
+                        content: {out: rid(parent), in: rid(grandparent1), source},
+                        existsOk: true,
+                        fetchExistsing: false
+                    });
+                }
+                if (grandparent2 && rid(parent) !== rid(grandparent2)) {
+                    await conn.addRecord({
+                        endpoint: 'subclassof',
+                        content: {out: rid(parent), in: rid(grandparent2), source},
+                        existsOk: true,
+                        fetchExistsing: false
+                    });
+                }
             }
             counts.success++;
         } catch (err) {
