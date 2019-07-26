0da20d82b04754108574bae7b6929e6819633c01
Caralyn Reisle <creisle@bcgsc.ca>
2019-07-26 11:55:23 -0700

    NCIT: Load additional semantic types

--- src/ncit.js
+++ src/ncit.js
@@ -6,7 +6,7 @@
  * @module importer/ncit
  */
 const {
-    loadDelimToJson, rid
+    loadDelimToJson, rid, generateCacheKey
 } = require('./util');
 const {logger} = require('./logging');
 
@@ -30,17 +30,21 @@ const anatomyConcepts = [
     'Anatomical Structure',
     'Body Location or Region',
     'Body Part, Organ, or Organ Component',
-    'Tissue',
+    'Tissue'
 ];
 
 const therapeuticConcepts = [
     'Antibiotic',
     'Biologically Active Substance',
+    'Chemical Viewed Functionally',
+    'Chemical Viewed Structurally',
+    'Chemical',
     'Immunologic Factor',
     'Inorganic Chemical',
     'Organic Chemical',
     'Pharmacologic Substance',
     'Therapeutic or Preventive Procedure',
+    'Vitamin'
 ];
 
 const DEPRECATED = [
@@ -66,7 +70,57 @@ const pickEndpoint = (conceptName) => {
         }
         endpoint = 'therapies';
     }
-    return endpoint;
+    if (endpoint) {
+        return endpoint;
+    }
+    throw new Error(`Concept not implemented (${conceptName})`);
+};
+
+/**
+ * Convert the raw row record to a standard form
+ */
+const cleanRawRow = (rawRow) => {
+    const {
+        id, synonyms, parents, xmlTag, name, definition, semanticType
+    } = rawRow;
+    const row = {
+        synonyms: [],
+        parents: [],
+        description: definition
+    };
+    const sourceId = id.toLowerCase().trim();
+    const endpoint = pickEndpoint(semanticType);
+
+    // use the synonym name if no name given
+    row.synonyms = synonyms.split('|')
+        .map(s => s.toLowerCase().trim())
+        .filter(s => s);
+
+    if (!row.name && row.synonyms.length > 0) {
+        row.synonyms.sort();
+        row.name = row.synonyms[0];
+    } else {
+        // if there is multiple names, demote the extra to synonyms
+        const names = name.split('|')
+            .map(s => s.toLowerCase().trim())
+            .filter(s => s);
+        names.sort();
+        row.name = names[0];
+        row.synonyms.push(...names.slice(1));
+    }
+
+    const url = xmlTag.replace(/^</, '').replace(/>$/, '');
+    const deprecated = parents.split('|').some(p => DEPRECATED.includes(p));
+
+    // add the parents
+    row.parents = parents.split('|')
+        .map(parent => parent.trim())
+        .filter(parent => parent && !DEPRECATED.includes(parent))
+        .map(parent => parent.toLowerCase());
+    row.synonyms = Array.from(new Set(row.synonyms)).filter(s => s !== row.name.toLowerCase().trim());
+    return {
+        ...row, url, deprecated, endpoint, sourceId
+    };
 };
 
 /**
@@ -97,102 +151,92 @@ const uploadFile = async ({filename, conn}) => {
         existsOk: true
     }));
     const recordsById = {};
-    const subclass = [];
-    const rawRecords = {};
+    const subclassEdges = [];
+
+    // list the ncit records already loaded
+    const cached = {};
+    logger.info('getting previously loaded records');
+    const cachedRecords = await conn.getRecords({
+        endpoint: 'ontologies',
+        where: {source, dependency: null, neighbors: 0}
+    });
+    for (const record of cachedRecords) {
+        cached[generateCacheKey(record)] = record;
+    }
+    logger.info(`loaded and cached ${Object.keys(cached).length} records`);
 
     const counts = {skip: 0, success: 0, error: 0};
-    const skippedWhy = {};
 
     for (let i = 0; i < rows.length; i++) {
+        logger.info(`processing ${rows[i].id}`);
         try {
-            const row = rows[i];
-            const sourceId = row.id.toLowerCase().trim();
-            if (recordsById[sourceId]) {
-                throw new Error(`code is not unique (${sourceId})`);
-            }
-            rawRecords[sourceId] = row;
-            const endpoint = pickEndpoint(row.semanticType);
-            if (!endpoint) {
-                skippedWhy[sourceId] = `semantic type (${row.semanticType})`;
-                counts.skip++;
-                continue;
-            }
-            // use the synonym name if no name given
-            const synonyms = row.synonyms.split('|')
-                .map(s => s.toLowerCase().trim())
-                .filter(s => s);
+            pickEndpoint(rows[i].semanticType);
+        } catch (err) {
+            logger.warn(err);
+            counts.skip++;
+            continue;
+        }
+        try {
+            const row = cleanRawRow(rows[i]);
 
-            if (!row.name) {
-                if (synonyms.length < 1) {
-                    counts.skip++;
-                    skippedWhy[sourceId] = 'no name';
-                    continue;
-                }
-                synonyms.sort();
-                row.name = synonyms[0];
+            const cacheKey = generateCacheKey(row);
+
+            if (recordsById[cacheKey]) {
+                throw new Error(`code is not unique (${cacheKey})`);
+            }
+
+            let record;
+            if (cached[cacheKey]) {
+                record = cached[cacheKey];
             } else {
-                // if there is multiple names, demote the extra to synonyms
-                const names = row.name.split('|')
-                    .map(s => s.toLowerCase().trim())
-                    .filter(s => s);
-                names.sort();
-                row.name = names[0];
-                synonyms.push(...names.slice(1));
-            }
-            logger.info(`processing ${row.id} (${i} of ${rows.length})`);
-
-            const url = row.xmlTag.replace(/^</, '').replace(/>$/, '');
-            const deprecated = row.parents.split('|').some(p => DEPRECATED.includes(p));
-            const record = await conn.addRecord({
-                endpoint,
-                content: {
-                    source,
-                    sourceId,
-                    name: row.name,
-                    description: row.definition,
-                    url,
-                    deprecated
-                },
-                fetchConditions: { // description can contain url malformed characters
-                    source,
-                    sourceId,
-                    name: row.name,
-                    dependency: null
-                },
-                existsOk: true
-            });
-
-            recordsById[sourceId] = rid(record);
-
-            // add the parents
-            subclass.push(
-                ...row.parents.split('|')
-                    .map(parent => parent.trim())
-                    .filter(parent => parent && !DEPRECATED.includes(parent))
-                    .map(parent => [sourceId, parent.toLowerCase()])
-            );
-
-            // add the synonyms
-            const uniqueSynonyms = Array.from(new Set(synonyms)).filter(s => s !== row.name.toLowerCase().trim());
-            await Promise.all(uniqueSynonyms.map(async (synonym) => {
-                const alias = await conn.addRecord({
+                // create the new record
+                const {
+                    endpoint, sourceId, description, url, name, deprecated
+                } = row;
+                record = await conn.addRecord({
                     endpoint,
                     content: {
                         source,
-                        sourceId: row.id,
-                        name: synonym,
-                        dependency: rid(record),
+                        sourceId,
+                        name,
+                        description,
+                        url,
                         deprecated
+                    },
+                    fetchConditions: { // description can contain url malformed characters
+                        source,
+                        sourceId,
+                        name: row.name,
+                        dependency: null
                     },
                     existsOk: true
                 });
-                await conn.addRecord({
-                    endpoint: 'aliasof',
-                    content: {out: rid(alias), in: rid(record), source},
-                    existsOk: true,
-                    fetchExisting: false
-                });
-            }));
+                cached[generateCacheKey(record)] = record;
+
+                // add the synonyms
+                await Promise.all(row.synonyms.map(async (synonym) => {
+                    const alias = await conn.addRecord({
+                        endpoint: row.endpoint,
+                        content: {
+                            source,
+                            sourceId: record.sourceId,
+                            name: synonym,
+                            dependency: rid(record),
+                            deprecated: record.deprecated
+                        },
+                        existsOk: true
+                    });
+                    await conn.addRecord({
+                        endpoint: 'aliasof',
+                        content: {out: rid(alias), in: rid(record), source},
+                        existsOk: true,
+                        fetchExisting: false
+                    });
+                }));
+            }
+
+            // add the parents
+            subclassEdges.push(row.parents.map(parent => [cacheKey, generateCacheKey({sourceId: parent})]));
             counts.success++;
         } catch (err) {
             logger.error(err);
@@ -201,22 +245,18 @@ const uploadFile = async ({filename, conn}) => {
     }
 
     // now create all the subclass relationships
-    for (const [record, parent] of subclass) {
-        if (recordsById[record] && recordsById[parent]) {
+    for (const [recordKey, parentKey] of subclassEdges) {
+        if (cached[recordKey] && cached[parentKey]) {
             await conn.addRecord({
                 endpoint: 'subclassof',
                 content: {
-                    out: recordsById[record],
-                    in: recordsById[parent],
+                    out: rid(cached[recordKey]),
+                    in: rid(cached[parentKey]),
                     source
                 },
                 existsOk: true,
                 fetchExisting: false
             });
-        } else if (skippedWhy[parent]) {
-            logger.info(`failed to link ${record} to ${parent}: ${skippedWhy[parent]}`);
-        } else {
-            logger.info(`failed to link ${record} to ${parent}`);
         }
     }
     logger.info(JSON.stringify(counts));
