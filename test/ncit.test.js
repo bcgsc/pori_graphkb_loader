c6d2fae1d68c72c1330bc3b73635c70154ed77bd
Cam Grisdale <cgrisdale@bcgsc.ca>
2021-09-10 13:50:13 -0700

    fixint eslint errors

--- test/ncit.test.js
+++ test/ncit.test.js
@@ -1,32 +1,28 @@
 // write tests for cleanRawRow and pickEndpoint
 
-const { clearnRawRow } = require('../src/ncit');
+const { cleanRawRow } = require('../src/ncit');
 const { pickEndpoint } = require('../src/ncit');
 
 describe('pickEndpoint', () => {
     test.each([
-        ['Islets of Langerhans','Tissue','AnatomicalEntity'],
-        ['Recurrent Childhood Giant Cell Glioblastoma','Neoplastic Process','Disease'],
-        ['Erlotinib','Pharmacologic Substance','Therapy'],
-        ['Nivolumab','Therapeutic or Preventive Procedure','Therapy'],
-        ['Vemurafenib','Organic Chemical|Pharmacologic Substance','Therapy'],
-
+        ['Islets of Langerhans', 'Tissue', 'AnatomicalEntity'],
+        ['Recurrent Childhood Giant Cell Glioblastoma', 'Neoplastic Process', 'Disease'],
+        ['Erlotinib', 'Pharmacologic Substance', 'Therapy'],
+        ['Nivolumab', 'Therapeutic or Preventive Procedure', 'Therapy'],
+        ['Vemurafenib', 'Organic Chemical|Pharmacologic Substance', 'Therapy'],
     ])(
         '%s|%s|%s returns %s', (conceptName, parentConcepts, expected) => {
             expect(pickEndpoint(conceptName, parentConcepts)).toEqual(expected);
         },
     );
-
 });
 
 describe('cleanRawRow', () => {
     test.each([
-        [],
+        ['', ''],
     ])(
         '%s|%s|%s returns %s', (rawRow, expected) => {
             expect(cleanRawRow(rawRow)).toEqual(expected);
         },
     );
-
 });
-
