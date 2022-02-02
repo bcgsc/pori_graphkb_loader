661e5337f86add25b1ecacc263a07818d71fce86
Mathieu Lemieux <mlemieux@bcgsc.ca>
2022-02-02 14:05:11 -0800

    Change <array>.forEach() to test.each()

--- test/graphkb.test.js
+++ test/graphkb.test.js
@@ -1,54 +1,68 @@
 const { simplifyRecordsLinks } = require('../src/graphkb');
 
 describe('simplifyRecordsLinks', () => {
-    test('simplifies nested objects into their RID only', () => {
-        const listOfContentToReturnAsIs = [
-            1.0,
-            null,
-        ];
-        const listOfContentToReturnUnnested = [
-            {},
-            { a: 1, b: 2 },
-            { '@rid': 123, a: 1 },
-            {
-                a: [
-                    { '@rid': 123, aa: 1 },
-                    { ab: 2 },
-                ],
-            },
-            {
+    const listOfContentToReturnAsIs = [
+        1.0,
+        null,
+    ];
+    const listOfContentToReturnUnnested = [
+        {},
+        { a: 1, b: 2 },
+        { '@rid': 123, a: 1 },
+        {
+            a: [
+                { '@rid': 123, aa: 1 },
+                { ab: 2 },
+            ],
+        },
+        {
+            a: { '@rid': 123, aa: 1 },
+            b: 2,
+        },
+        {
+            a: {
+                '@rid': 123,
                 a: { '@rid': 123, aa: 1 },
-                b: 2,
             },
-            {
-                a: {
-                    '@rid': 123,
-                    a: { '@rid': 123, aa: 1 },
-                },
-            },
-        ];
-        const ListOfReturnedUnnestedContent = [
-            {},
-            { a: 1, b: 2 },
-            { '@rid': 123, a: 1 },
-            {
-                a: [
-                    '123',
-                    { ab: 2 },
-                ],
-            },
-            { a: '123', b: 2 },
-            { a: '123' },
-        ];
+        },
+    ];
+    const ListOfReturnedUnnestedContent = [
+        {},
+        { a: 1, b: 2 },
+        { '@rid': 123, a: 1 },
+        {
+            a: [
+                '123',
+                { ab: 2 },
+            ],
+        },
+        { a: '123', b: 2 },
+        { a: '123' },
+    ];
 
-        listOfContentToReturnAsIs.forEach(content => {
-            const output = simplifyRecordsLinks(content);
-            expect(output).toBe(content);
-        });
-        listOfContentToReturnUnnested.forEach((content, index) => {
-            const output = simplifyRecordsLinks(content);
-            const expectedOutput = simplifyRecordsLinks(ListOfReturnedUnnestedContent[index]);
-            expect(output).toEqual(expectedOutput);
-        });
+    test.each(listOfContentToReturnAsIs)('do nothing when content not a list/k-v pair', content => {
+        const output = simplifyRecordsLinks(content);
+        expect(output).toBe(content);
+    });
+
+    const cases = [];
+    listOfContentToReturnUnnested.forEach((content, index) => {
+        cases.push([content, ListOfReturnedUnnestedContent[index]]);
+    });
+
+    test.each(cases)('simplifies nested obj. to RID', (content, expectedOutput) => {
+        const output = simplifyRecordsLinks(content);
+        expect(output).toEqual(expectedOutput);
     });
 });
+
+
+
+
+
+
+
+
+
+
+//
