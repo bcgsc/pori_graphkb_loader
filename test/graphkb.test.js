cded28a936a2b7872dee56b156c8ef39481f909e
Mathieu Lemieux <mlemieux@bcgsc.ca>
2022-02-03 11:24:56 -0800

    Simplify test.each structure

--- test/graphkb.test.js
+++ test/graphkb.test.js
@@ -1,68 +1,36 @@
 const { simplifyRecordsLinks } = require('../src/graphkb');
 
 describe('simplifyRecordsLinks', () => {
-    const listOfContentToReturnAsIs = [
-        1.0,
+    test.each([
+        123,
+        123.0,
+        'abc',
         null,
-    ];
-    const listOfContentToReturnUnnested = [
+        undefined,
+        false,
         {},
-        { a: 1, b: 2 },
+        { a: 1, b: 1 },
         { '@rid': 123, a: 1 },
-        {
-            a: [
-                { '@rid': 123, aa: 1 },
-                { ab: 2 },
-            ],
-        },
-        {
-            a: { '@rid': 123, aa: 1 },
-            b: 2,
-        },
-        {
-            a: {
-                '@rid': 123,
-                a: { '@rid': 123, aa: 1 },
-            },
-        },
-    ];
-    const ListOfReturnedUnnestedContent = [
-        {},
-        { a: 1, b: 2 },
-        { '@rid': 123, a: 1 },
-        {
-            a: [
-                '123',
-                { ab: 2 },
-            ],
-        },
-        { a: '123', b: 2 },
-        { a: '123' },
-    ];
-
-    test.each(listOfContentToReturnAsIs)('do nothing when content not a list/k-v pair', content => {
-        const output = simplifyRecordsLinks(content);
-        expect(output).toBe(content);
+    ])('does not change', (inputValue) => {
+        const output = simplifyRecordsLinks(inputValue);
+        expect(output).toEqual(inputValue);
     });
 
-    const cases = [];
-    listOfContentToReturnUnnested.forEach((content, index) => {
-        cases.push([content, ListOfReturnedUnnestedContent[index]]);
-    });
-
-    test.each(cases)('simplifies nested obj. to RID', (content, expectedOutput) => {
-        const output = simplifyRecordsLinks(content);
-        expect(output).toEqual(expectedOutput);
+    test.each([
+        [
+            { a: [{ '@rid': 123, aa: 1 }, { ab: 2 }] },
+            { a: ['123', { ab: 2 }] },
+        ],
+        [
+            { a: { '@rid': 123, aa: 1 }, b: 2 },
+            { a: '123', b: 2 },
+        ],
+        [
+            { a: { '@rid': 123, a: { '@rid': 123, aa: 1 } } },
+            { a: '123' },
+        ],
+    ])('being unnested', (inputValue, expectedValue) => {
+        const output = simplifyRecordsLinks(inputValue);
+        expect(output).toEqual(expectedValue);
     });
 });
-
-
-
-
-
-
-
-
-
-
-//
