947a9a6a1635589c9c131d7aed41fa92949bd269
Mathieu Lemieux <mlemieux@bcgsc.ca>
2022-04-19 11:14:52 -0700

    Modify test for Ensembl loader fetchAndLoadById()

--- test/ensembl/ensembl.fetchAndLoadById.test.js
+++ test/ensembl/ensembl.fetchAndLoadById.test.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const fetchMock = require('node-fetch');
 const { fetchAndLoadById } = require('../../src/ensembl');
 const { ApiConnection } = require('../../src/graphkb');
@@ -11,20 +12,16 @@ jest.mock('../../src/graphkb', () => {
     return { ApiConnection: mockApiConnection, ...graphkb };
 });
 
+const extApiData = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/external_api_responses.json')));
+Object.entries(extApiData).forEach(([url, respBody]) => {
+    fetchMock.get(url, respBody);
+});
+
+
 describe('fetchAndLoadById in Ensembl loader', () => {
-    // Arguments matching mock data
-    const biotype = 'transcript';
-    const sourceId = 'ENST00000544455';
-    const sourceIdVersion = '6';
-    const ensemblUrl = `http://rest.ensembl.org/lookup/id/${sourceId.toLowerCase()}`;
-    const sourceIdGene = 'ENSG00000139618';
-
-    // Mock response of node-fetch module for request to ensembl API
-    fetchMock.get(ensemblUrl, () => ({ Parent: sourceIdGene }));
-
-    // Mock response of ApiConnection's request method for request to graphKb API
-    const mockFile = `${process.cwd()}/test/data/ensembl_fetchAndLoadById_requests.json`;
-    const mockDataset = JSON.parse(fs.readFileSync(mockFile, { encoding: 'utf-8', flag: 'r' }));
+    // Mock response of ApiConnection's request method for requests to GraphKB API
+    const mockFile = path.join(__dirname, '../data/ensembl_fetchAndLoadById_requests.json');
+    const mockDataset = JSON.parse(fs.readFileSync(mockFile));
     const conn = new ApiConnection('');
     conn.request.mockImplementation(() => {
         const { result } = mockDataset[0];
@@ -32,13 +29,16 @@ describe('fetchAndLoadById in Ensembl loader', () => {
         return result;
     });
 
-    // Returned RIDs from mock responses
+    // Returned RIDs from GraphKB mock responses
     const ridSource = mockDataset[1].result.result['@rid'];
     const ridTranscript = mockDataset[3].result.result['@rid'];
     const ridUnversionizedTranscript = mockDataset[4].result.result['@rid'];
     const ridGene = mockDataset[6].result.result['@rid'];
 
     // Calling fetchAndLoadById
+    const biotype = 'transcript';
+    const sourceId = 'ENST00000544455';
+    const sourceIdVersion = '6';
     fetchAndLoadById(conn, { biotype, sourceId, sourceIdVersion });
 
     // TEST SUITE
@@ -86,12 +86,12 @@ describe('fetchAndLoadById in Ensembl loader', () => {
     });
 
     test('Add gene Feature (from Ensembl API) and ElementOf', () => {
-        expect(fetchMock).toHaveFetched(ensemblUrl);
+        expect(fetchMock).toHaveFetched(`http://rest.ensembl.org/lookup/id/${sourceId.toLowerCase()}`);
         expect(conn.request).toHaveBeenCalledWith(expect.objectContaining({
             body: expect.objectContaining({
                 biotype: 'gene',
                 source: ridSource,
-                sourceId: sourceIdGene,
+                sourceId: 'ENSG00000139618',
                 sourceIdVersion: null,
             }),
             method: 'POST',
