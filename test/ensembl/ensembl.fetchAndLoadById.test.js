2c9a95191929a78f1dfe9d2f49730518d07a74bf
Mathieu Lemieux <mlemieux@bcgsc.ca>
2022-04-19 16:09:04 -0700

    Switch to class mocking

--- test/ensembl/ensembl.fetchAndLoadById.test.js
+++ test/ensembl/ensembl.fetchAndLoadById.test.js
@@ -4,45 +4,45 @@ const fetchMock = require('node-fetch');
 const { fetchAndLoadById } = require('../../src/ensembl');
 const { ApiConnection } = require('../../src/graphkb');
 
+// Mock node-fetch calls to external APIs
 jest.mock('node-fetch', () => require('fetch-mock-jest').sandbox()); // eslint-disable-line global-require
-
-jest.mock('../../src/graphkb', () => {
-    const { ApiConnection: mockApiConnection, ...graphkb } = jest.requireActual('../../src/graphkb');
-    mockApiConnection.prototype.request = jest.fn();
-    return { ApiConnection: mockApiConnection, ...graphkb };
-});
-
 const extApiData = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/external_api_responses.json')));
 Object.entries(extApiData).forEach(([url, respBody]) => {
     fetchMock.get(url, respBody);
 });
 
+// Mock ApiConnexion class with overrided request function
+class MockApiConnection extends ApiConnection {
+    constructor() {
+        super();
+        this.mockFile = path.join(__dirname, '../data/ensembl_fetchAndLoadById_requests.json');
+        this.mockDataset = JSON.parse(fs.readFileSync(this.mockFile));
+        this.ridSource = this.mockDataset[1].result.result['@rid'];
+        this.ridTranscript = this.mockDataset[3].result.result['@rid'];
+        this.ridUnversionizedTranscript = this.mockDataset[4].result.result['@rid'];
+        this.ridGene = this.mockDataset[6].result.result['@rid'];
+    }
+
+    request() {
+        const { result } = this.mockDataset[0];
+        this.mockDataset.shift();
+        return result;
+    }
+}
 
 describe('fetchAndLoadById in Ensembl loader', () => {
-    // Mock response of ApiConnection's request method for requests to GraphKB API
-    const mockFile = path.join(__dirname, '../data/ensembl_fetchAndLoadById_requests.json');
-    const mockDataset = JSON.parse(fs.readFileSync(mockFile));
-    const conn = new ApiConnection('');
-    conn.request.mockImplementation(() => {
-        const { result } = mockDataset[0];
-        mockDataset.shift();
-        return result;
+    const conn = new MockApiConnection(),
+        biotype = 'transcript',
+        sourceId = 'ENST00000544455',
+        sourceIdVersion = '6';
+
+    beforeAll(async () => {
+        jest.spyOn(conn, 'request');
+        await fetchAndLoadById(conn, { biotype, sourceId, sourceIdVersion });
     });
 
-    // Returned RIDs from GraphKB mock responses
-    const ridSource = mockDataset[1].result.result['@rid'];
-    const ridTranscript = mockDataset[3].result.result['@rid'];
-    const ridUnversionizedTranscript = mockDataset[4].result.result['@rid'];
-    const ridGene = mockDataset[6].result.result['@rid'];
-
-    // Calling fetchAndLoadById
-    const biotype = 'transcript';
-    const sourceId = 'ENST00000544455';
-    const sourceIdVersion = '6';
-    fetchAndLoadById(conn, { biotype, sourceId, sourceIdVersion });
-
     // TEST SUITE
-    test('Add Ensembl Source', () => {
+    test('Add Ensembl Source', async () => {
         expect(conn.request).toHaveBeenCalledWith(expect.objectContaining({
             body: expect.objectContaining({ name: 'ensembl' }),
             method: 'POST',
@@ -54,7 +54,7 @@ describe('fetchAndLoadById in Ensembl loader', () => {
         expect(conn.request).toHaveBeenCalledWith(expect.objectContaining({
             body: expect.objectContaining({
                 biotype,
-                source: ridSource,
+                source: conn.ridSource,
                 sourceId,
                 sourceIdVersion,
             }),
@@ -67,7 +67,7 @@ describe('fetchAndLoadById in Ensembl loader', () => {
         expect(conn.request).toHaveBeenCalledWith(expect.objectContaining({
             body: expect.objectContaining({
                 biotype,
-                source: ridSource,
+                source: conn.ridSource,
                 sourceId: sourceId.toLowerCase(),
                 sourceIdVersion: null,
             }),
@@ -76,9 +76,9 @@ describe('fetchAndLoadById in Ensembl loader', () => {
         }));
         expect(conn.request).toHaveBeenCalledWith(expect.objectContaining({
             body: expect.objectContaining({
-                in: ridTranscript,
-                out: ridUnversionizedTranscript,
-                source: ridSource,
+                in: conn.ridTranscript,
+                out: conn.ridUnversionizedTranscript,
+                source: conn.ridSource,
             }),
             method: 'POST',
             uri: '/generalizationof',
@@ -90,7 +90,7 @@ describe('fetchAndLoadById in Ensembl loader', () => {
         expect(conn.request).toHaveBeenCalledWith(expect.objectContaining({
             body: expect.objectContaining({
                 biotype: 'gene',
-                source: ridSource,
+                source: conn.ridSource,
                 sourceId: 'ENSG00000139618',
                 sourceIdVersion: null,
             }),
@@ -99,9 +99,9 @@ describe('fetchAndLoadById in Ensembl loader', () => {
         }));
         expect(conn.request).toHaveBeenCalledWith(expect.objectContaining({
             body: expect.objectContaining({
-                in: ridGene,
-                out: ridUnversionizedTranscript,
-                source: ridSource,
+                in: conn.ridGene,
+                out: conn.ridUnversionizedTranscript,
+                source: conn.ridSource,
             }),
             method: 'POST',
             uri: '/elementof',
