35ac6b7bbea51595032eb48c89bc0176d706cfef
mathieulemieux <mathieu@mathieulemieux.com>
2024-05-27 16:15:12 -0700

    Add noUpdate flag to civic parser

--- bin/load.js
+++ bin/load.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
 const { runLoader } = require('../src');
 const { createOptionsMenu, fileExists } = require('../src/cli');
 
@@ -36,7 +33,12 @@ const cosmicResistance = require('../src/cosmic/resistance');
 const cosmicFusions = require('../src/cosmic/fusions');
 
 const API_MODULES = {
-    asco, dgidb, docm, fdaApprovals, moa, oncotree,
+    asco,
+    dgidb,
+    docm,
+    fdaApprovals,
+    moa,
+    oncotree,
 };
 
 const FILE_MODULES = {
@@ -101,6 +103,11 @@ civicParser.add_argument('--trustedCurators', {
     default: [],
     help: 'CIViC User IDs of curators whose statements should be imported even if they have not yet been reviewed (evidence is submitted but not accepted)',
     nargs: '+',
+});
+civicParser.add_argument('--noUpdate', {
+    action: 'store_true',
+    default: false,
+    help: 'Will not check for updating content of existing GraphKB Statements',
 });
 
 const clinicaltrialsgovParser = subparsers.add_parser('clinicaltrialsgov');
